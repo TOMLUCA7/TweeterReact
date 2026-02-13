4a5b738c63b882992863de39214696d795412007
TOMLUCA7 <your-github-email@example.com>
2026-02-13 11:16:37 +0200

    feat: Add user name input and save button to the profile page.

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+import { TextInput, Button } from "@mantine/core";
+
 const ProfilePage = () => {
+  const [userName, setUserName] = useState("");
+
   return (
     <>
-      <h1>ProfilePage</h1>
+      <TextInput
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+        style={{
+          width: "50%",
+          marginBottom: "30px",
+          marginTop: "30px",
+          marginLeft: "30px",
+        }}
+        label="User Name"
+        placeholder="Enter your name"
+      />
+      <Button
+        style={{
+          width: "10%",
+          marginBottom: "30px",
+          marginTop: "30px",
+          marginLeft: "30px",
+        }}
+      >
+        Save
+      </Button>
     </>
   );
 };
